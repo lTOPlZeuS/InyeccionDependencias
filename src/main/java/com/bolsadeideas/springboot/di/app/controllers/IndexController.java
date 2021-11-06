be243b52b36ceefbf7479da69d22fd54fda9d7ac
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-05 18:59:50 -0600

    Desacoplando inyeccion de dependencias por interfas

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,6 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
-import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
   @Autowired
-  private MiServicio miServicio;
-  
+  private IServicio  miServicio;
+
   @GetMapping({"/","","/index"})
   public String index(Model model) {
     //Enviando un recurso sin utilizar inyección de dependencias
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -2,8 +2,9 @@ package com.bolsadeideas.springboot.di.app.models.service;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class MiServicio {
+@Component("miServicioSimple")
+public class MiServicio implements IServicio {
+  @Override
   public String operacion(){
     return "Ejecutando un proceso importante ...";
   }
