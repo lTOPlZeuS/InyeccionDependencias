c79dc7d84cb60d5f7c7164897e63b093f3606076
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-05 18:00:14 -0600

    Agregando service y mostrandolo en front

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,11 +1,18 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
+import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+  private MiServicio miServicio = new MiServicio();
+  @GetMapping({"/","","/index"})
   public String index(Model model) {
+    //Enviando un recurso sin utilizar inyección de dependencias
+    model.addAttribute("objeto", miServicio.operacion());
     return "index";
   }
 }
