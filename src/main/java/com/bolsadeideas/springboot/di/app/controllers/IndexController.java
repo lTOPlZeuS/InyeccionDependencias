8c3b714ebaef905b5499c7e12831a631e48c36e7
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-05 18:36:05 -0600

    Agregando iinyeccion de dependecias con el principio hollywood

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -2,13 +2,16 @@ package com.bolsadeideas.springboot.di.app.controllers;
 
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-  private MiServicio miServicio = new MiServicio();
+  @Autowired
+  private MiServicio miServicio;
+  
   @GetMapping({"/","","/index"})
   public String index(Model model) {
     //Enviando un recurso sin utilizar inyección de dependencias
